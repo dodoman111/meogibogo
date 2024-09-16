f25c9638f392281dff75f40de8b36f208435ecca
daehwan <dodoman111@naver.com>
2024-09-17 02:50:47 +0900

    feat: 메인 페이지 추가

--- food/views.py
+++ food/views.py
@@ -1,3 +1,7 @@
+# Create your views here.
+from django.http import HttpResponse
 from django.shortcuts import render
 
-# Create your views here.
+
+def home(request):
+    return HttpResponse("Welcome to 먹이보고!")
